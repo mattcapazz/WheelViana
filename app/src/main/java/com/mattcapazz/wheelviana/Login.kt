e68a0f0a0f2dcd0c1eaa6341bb9ffd3cb66e41e3
paulo14052 <72792890+paulo14052@users.noreply.github.com>
2021-11-13 19:40:12 +0000

    Atualização Login/resgistar

--- app/src/main/java/com/mattcapazz/wheelviana/Login.kt
+++ app/src/main/java/com/mattcapazz/wheelviana/Login.kt
@@ -62,11 +62,21 @@ class Login : AppCompatActivity() {
     val password = findViewById<EditText>(R.id.password).text.toString()
 
 
-     val text = "Hello toast!"
-    val duration = Toast.LENGTH_SHORT
-
-    val toast = Toast.makeText(applicationContext, text, duration)
-    toast.show()
+    auth.signInWithEmailAndPassword(email, password)
+      .addOnCompleteListener(this) { task ->
+        if (task.isSuccessful) {
+          // Sign in success, update UI with the signed-in user's information
+          Log.d(TAG, "signInWithEmail:success")
+          val user = auth.currentUser
+          //updateUI(user)
+        } else {
+          // If sign in fails, display a message to the user.
+          Log.w(TAG, "signInWithEmail:failure", task.exception)
+          Toast.makeText(baseContext, "Authentication failed.",
+            Toast.LENGTH_SHORT).show()
+          // updateUI(null)
+        }
+      }
   }
 
   }
