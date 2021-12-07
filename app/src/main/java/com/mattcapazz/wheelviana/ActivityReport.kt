be79562da5a40ff910d331788b454d087b9e45cb
paulo14052 <72792890+paulo14052@users.noreply.github.com>
2021-12-07 15:11:46 +0000

    report

---
+++ app/src/main/java/com/mattcapazz/wheelviana/ActivityReport.kt
@@ -0,0 +1,14 @@
+package com.mattcapazz.wheelviana
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityReport : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_report)
+    }
+}
