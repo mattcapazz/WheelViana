3b7a6ae08697706c6fc7a0e9407c438d4402acb9
Mario Fernandes <m4r10i@gmail.com>
2021-12-09 23:25:27 +0000

    Troca de posiçao no botao do calendario e o switch Adicionar Textview a direita do botao calendario para mostrar a data selecionada.

--- app/src/main/java/com/mattcapazz/wheelviana/MainActivity.kt
+++ app/src/main/java/com/mattcapazz/wheelviana/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.mattcapazz.wheelviana
 
 import android.annotation.SuppressLint
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.MotionEvent
+import android.view.View
 import android.view.View.OnTouchListener
 import android.widget.EditText
 import android.widget.Toast
@@ -12,6 +14,11 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
+import java.util.*
+import android.widget.TextView
+
+
+
 
 class MainActivity : AppCompatActivity() {
   @SuppressLint("ClickableViewAccessibility")
@@ -20,6 +27,9 @@ class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+
+
 
     val drawer = findViewById<DrawerLayout>(R.id.drawerLayout)
     val nav = findViewById<NavigationView>(R.id.navView)
@@ -82,4 +92,20 @@ class MainActivity : AppCompatActivity() {
   fun maps(view: android.view.View) {
     startActivity(Intent(this, Maps::class.java))
   }
+
+  fun calendar(view: android.view.View) {
+    val dateTv = findViewById<View>(R.id.dateTv) as TextView
+    val c= Calendar.getInstance()
+    val year= c.get(Calendar.YEAR)
+    val month = c.get(Calendar.MONTH)
+    val day = c.get(Calendar.DAY_OF_MONTH)
+    var dpd = DatePickerDialog(this,DatePickerDialog.OnDateSetListener { view, mYear,mMonth , mDay ->
+      val mmMonth = mMonth+1
+      val date = "$mDay/$mmMonth/$mYear"
+      dateTv.setText(date)
+    },year,month,day)
+    dpd.show()
+  }
+
+
 }
